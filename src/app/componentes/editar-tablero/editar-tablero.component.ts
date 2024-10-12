8c7fcbdfbfc2effff3118a124b37187c35987d7f
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-12 10:52:10 -0300

    Creación de componentes e instalación de firebase.

---
+++ src/app/componentes/editar-tablero/editar-tablero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-tablero',
+  standalone: true,
+  imports: [],
+  templateUrl: './editar-tablero.component.html',
+  styleUrl: './editar-tablero.component.css'
+})
+export class EditarTableroComponent {
+
+}
