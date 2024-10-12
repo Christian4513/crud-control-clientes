8991e70ae9ac49bd69e702d1bb2720856bfc239c
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-12 11:06:40 -0300

    configuracion de rutas y corrección en componentes mal escritos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { TableroComponent } from './componentes/tablero/tablero.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
+import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: TableroComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'registrarse', component: RegistroComponent},
+  {path: 'configuracion', component: ConfiguracionComponent},
+  {path: 'registrarse', component: RegistroComponent},
+  {path: 'cliente/editar/:id', component: EditarClienteComponent},
+  {path: '**', component: NoEncontradoComponent},
+];
