6a5cd3fc404b86049643844f964db2855266812a
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-13 13:25:24 -0300

    Creación de header y footer

--- src/app/componentes/tablero/tablero.component.ts
+++ src/app/componentes/tablero/tablero.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ClientesComponent } from "../clientes/clientes.component";
+import { CabeceroComponent } from "../cabecero/cabecero.component";
 
 @Component({
   selector: 'app-tablero',
   standalone: true,
-  imports: [],
+  imports: [ClientesComponent, CabeceroComponent],
   templateUrl: './tablero.component.html',
   styleUrl: './tablero.component.css'
 })
