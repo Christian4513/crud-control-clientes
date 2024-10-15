ff9e83af4ac15c641416b3de2985e7052d41886a
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-15 13:43:19 -0300

    Conexion a base de datos, servicio clientes sin errores.

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Cliente } from '../modelo/cliente.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ClienteService {
+export class ClienteServicio {
+  clientesColeccion!: AngularFirestoreCollection<Cliente>;
+  clienteDoc!: AngularFirestoreDocument<Cliente>;
+  clientes!: Observable<Cliente[]>;
+  cliente!: Observable<Cliente>;
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    this.clientesColeccion = db.collection('clientes', (ref) =>
+      ref.orderBy('nombre', 'asc')
+    );
+  }
+
+  getClientes(): Observable<Cliente[]> {
+    this.clientes = this.clientesColeccion.snapshotChanges().pipe(
+      map((cambios) => {
+        return cambios.map((accion) => {
+          const datos = accion.payload.doc.data() as Cliente;
+          datos.id = accion.payload.doc.id;
+          return datos;
+        });
+      })
+    );
+
+    return this.clientes;
+  }
 }
