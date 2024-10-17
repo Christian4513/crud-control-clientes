1bf4af63e983d773567067d29b4ba87c56d9596c
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-16 22:20:49 -0300

    Instalacion de ngx-toastr (mensajes flash) y su configuración.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import {provideAnimations} from '@angular/platform-browser/animations'
+import {provideToastr} from 'ngx-toastr'
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [ provideAnimations(), provideToastr({timeOut: 900, preventDuplicates: true}), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+
+
+  ]
 };
+
+
--- src/app/componentes/clientes/clientes.component.ts
+++ src/app/componentes/clientes/clientes.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { ClienteServicio } from '../../servicios/cliente.service';
@@ -7,42 +7,42 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { Cliente } from '../../modelo/cliente.model';
 import { RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-clientes',
   standalone: true,
-  imports: [FormsModule, CommonModule, AngularFireModule, AngularFirestoreModule, RouterModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    AngularFireModule,
+    AngularFirestoreModule,
+    RouterModule,
+  ],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css',
-  providers: [ClienteServicio]
+  providers: [ClienteServicio],
 })
 export class ClientesComponent implements OnInit {
+  clientes: Cliente[] = [];
 
-  clientes!: Cliente[];
+  toastSvc = inject(ToastrService);
 
-  constructor(private clientesServicio: ClienteServicio){}
+  constructor(private clientesServicio: ClienteServicio) {}
 
   ngOnInit() {
-    this.clientesServicio.getClientes().subscribe(
-      clientes => {
-        this.clientes = clientes;
-      }
-    )
+    this.clientesServicio.getClientes().subscribe((clientes) => {
+      this.clientes = clientes;
+    });
   }
   getSaldoTotal() {
     let saldoTotal: number = 0;
     if (this.clientes) {
-      this.clientes.forEach(cliente => {
-        saldoTotal += cliente.saldo;  // Suma el saldo, usa 0 si saldo es null o undefined
+      this.clientes.forEach((cliente) => {
+        saldoTotal += cliente.saldo; // Suma el saldo, usa 0 si saldo es null o undefined
       });
     }
-    return saldoTotal;  // Retorna el saldo total
-}
-
-
-
+    return saldoTotal; // Retorna el saldo total
+  }
 
 }
-
-
-
