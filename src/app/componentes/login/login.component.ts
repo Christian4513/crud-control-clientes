d68a6a795985ad068be00a767bd3e8de40496264
Christian Fuentes <christian.fuenteszelada@gmail.com>
2024-10-19 16:55:25 -0300

    Se agrega login con su logica.

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoginService } from '../../servicios/login.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email!: string;
+  password!: string;
 
+  toastSvc = inject(ToastrService); // Inyección del servicio ToastrService para mostrar notificaciones
+  router = inject(Router);
+  loginService = inject(LoginService);
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  login() {
+    this.loginService
+      .login(this.email, this.password)
+      .then((res) => {
+        this.router.navigate(['/']);
+      })
+      .catch((error) => {
+        this.toastSvc.error(
+          'No has iniciado sesión',
+          'Error de validación',
+          {
+            timeOut: 4000,
+            positionClass: 'toast-top-right',
+          }
+        );
+      }); // Asegúrate de cerrar correctamente el catch
+  }
 }
---
+++ src/app/servicios/login.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  authService = inject(AngularFireAuth);
+
+  login(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.authService
+        .signInWithEmailAndPassword(email, password)
+        .then((datos) => resolve(datos))
+        .catch((error) => reject(error));
+    });
+  }
+}
